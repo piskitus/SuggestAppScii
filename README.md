25729fc5e15ddbb28c6323d704679d343c40cc12
piskitus <marcalarcon1994@gmail.com>
2016-12-30 10:06:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Para arrancar el proyecto:
+
+1) $npm install
+
+2) cd client (para entrar en la carpeta cliente) y $bower install
+
+3) $npm start (para arrancar el server en localhost:3000)
---
+++ client/main.js
@@ -0,0 +1,51 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/main.html',
+      access: {restricted: false}
+    })
+      .when('/home', {
+        templateUrl: 'partials/home.html',
+        access: {restricted: true}
+      })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'loginController',
+      access: {restricted: false}
+    })
+    .when('/logout', {
+      controller: 'logoutController',
+      access: {restricted: true}
+    })
+    .when('/register', {
+      templateUrl: 'partials/register.html',
+      controller: 'registerController',
+      access: {restricted: false}
+    })
+      .when('/restringido', {
+          template: '<h1>A Esta página solo se puede acceder logueado!</h1>',
+          access: {restricted: true}
+      })
+      .when('/norestringido', {
+          template: '<h1>A esta página se puede acceder aunque no estés logueado</h1>',
+          access: {restricted: false}
+      })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+myApp.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+    function (event, next, current) {
+      AuthService.getUserStatus()
+      .then(function(){
+        if (next.access.restricted && !AuthService.isLoggedIn()){
+          $location.path('/login');
+          $route.reload();
+        }
+      });
+  });
+});
