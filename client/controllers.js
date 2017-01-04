0b4ad866b30c62938924acd2a8c2b2655f1c8b70
piskitus <marcalarcon1994@gmail.com>
2017-01-04 12:58:25 +0100

    pequeños cambios

--- client/controllers.js
+++ client/controllers.js
@@ -1,13 +1,12 @@
 angular.module('myApp').controller('loginController',
   ['$scope', '$location', 'AuthService',
   function ($scope, $location, AuthService) {
-
     $scope.login = function () {
-
 
       // initial values
       $scope.error = false;
       $scope.disabled = true;
+
 
       // call login from service
       AuthService.login($scope.loginForm.username, $scope.loginForm.password)
@@ -34,7 +33,6 @@ angular.module('myApp').controller('logoutController',
   function ($scope, $location, AuthService) {
 
     $scope.logout = function () {
-
       // call logout from service
       AuthService.logout()
         .then(function () {
